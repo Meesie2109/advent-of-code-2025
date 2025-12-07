74113cdf69f41b99f87f868b64075f3bb04481c4
Meesie2109 <meesrootjes@icloud.com>
2025-12-07 22:15:44 +0100

    feat(day4): add iterative removal simulation for Part 2

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -1,3 +1,5 @@
+use std::result;
+
 pub fn solve_part1(input: &str) -> Result<i32, Box<dyn std::error::Error>> {
     let lines: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
 
@@ -52,4 +54,53 @@ fn count_neighbors(idx: usize, rows: [&[char]; 3]) -> usize {
     }
 
     count
+}
+
+pub fn solve_part2(input: &str) -> Result<i32, Box<dyn std::error::Error>> {
+    let mut lines: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+
+    let mut result = 0;
+    loop {
+        let removed_this_round = simulate_step(&mut lines);
+        if removed_this_round < 1 {
+            break;
+        } else {
+            result += removed_this_round;
+        }
+    }
+
+    Ok(result)
+}
+
+pub fn simulate_step(lines: &mut Vec<Vec<char>>) -> i32 {
+    let height = lines.len();
+    let width = lines[0].len();
+
+    let mut result = 0;
+
+    for y in 0..height {
+        for x in 0..width {
+            if lines[y][x] != '@' {
+                continue;
+            }
+
+            let prev = if y > 0 { &lines[y - 1] } else { &Vec::new() };
+            let curr = &lines[y];
+            let next = if y + 1 < height {
+                &lines[y + 1]
+            } else {
+                &Vec::new()
+            };
+
+            let neighbors = count_neighbors(x, [prev, curr, next]);
+
+            if neighbors < 4 {
+                result += 1;
+
+                lines[y][x] = '.';
+            }
+        }
+    }
+
+    result
 }
